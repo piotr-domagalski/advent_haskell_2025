e76f92eda01e5e68fed14506f4bd0ffee1e2be41
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-02 06:26:53 +0100

    Code golf: 3-SLOC day 1 task 1 solution

--- day01/minimal.hs
+++ day01/minimal.hs
@@ -1,12 +1,9 @@
-main' :: String -> IO ()
-main' file = do
-   ans1 <- solve1 . parse <$> readFile file
-   putStrLn $ show ans1
+import System.Environment
 
-parse :: String -> [Int]
+main = getArgs >>= main' . head
+
+main' file = show . solve1 . parse <$> readFile file >>= putStrLn
+
 parse = map (read . \(x:xs) -> (if x == 'L' then '-' else ' '):xs) . lines
 
--- solve :: [Int] -> Int
-solve1 = length
-      . filter (==0)
-      . scanl (\acc v -> (acc+v) `mod` 100) 50
+solve1 = length . filter (==0) . scanl (\acc v -> (acc+v) `mod` 100) 50
