ceead7fe84f6cd0a786d999b96da5937877b1bfe
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-04 16:16:21 +0100

    day 4: Grid.hs - Functor, Foldable, 3x3 windower; task1 solution

--- day04/Grid.hs
+++ day04/Grid.hs
@@ -1,5 +1,8 @@
-module Grid ( fromList
-            , Grid
+module Grid ( Grid(..)
+            , fromList
+            , toList
+            , gridWindowsFill3x3
+            , gridAt
             ) where
 
 import Data.List(unfoldr)
@@ -12,6 +15,12 @@ instance (Show a) => Show (Grid a) where
                           . unlines
                           . map show
                           . getRows
+
+instance Functor Grid where
+    fmap fn (Grid w h l) = (Grid w h (map fn l))
+
+instance Foldable Grid where
+    foldr fn acc (Grid _ _ l) = foldr fn acc l
 
 getRows :: Grid a -> [[a]]
 getRows (Grid w h l) = unfoldr aux l
@@ -30,3 +39,35 @@ fromList w h l
     where error_length_string = showDims (Grid w h []) ++
                                 " can only be constructed from a list of length " ++ (show (w*h)) ++
                                 "; got: " ++ (show $ length l)
+
+toList :: Grid a -> [a]
+toList (Grid _ _ l) = l
+
+gridAt :: Int -> Int -> Grid a -> Maybe a
+gridAt x y (Grid w h l) 
+    | x >= 0 && x < w && y >= 0 && y < h = Just (l !! (y*w+x))
+    | otherwise = Nothing
+
+listWindows :: Int -> [a] -> [[a]]
+listWindows n [] = []
+listWindows n list@(_:tail)
+    | length list < n = []
+    | otherwise = (take n list):(listWindows n tail)
+
+listWindowsFill :: Int -> a -> [a] -> [[a]]
+listWindowsFill n fill list = listWindows n (edge ++ list ++ edge)
+    where edge = replicate (n`div`2) fill
+
+gridWindowsFill3x3 :: a -> Grid a -> [Grid a]
+gridWindowsFill3x3 fill grid@(Grid w h l) = windowList
+    where rowWins = map (listWindowsFill 3 fill) $ getRows grid
+          row_empty = replicate w (replicate 3 fill)
+          rowWinsFilled = row_empty:rowWins ++ [row_empty]
+          rowWinsFilled' = concat rowWinsFilled
+          windowTuples = zip3 rowWinsFilled' (drop w rowWinsFilled') (drop (2*w) rowWinsFilled')
+          windowList = map (\(a,b,c) -> fromList 3 3 (a++b++c)) windowTuples
+
+gridIntsSquare :: Int -> Grid Int
+gridIntsSquare n = fromList n n [1..n*n]
+gridInts3 = gridIntsSquare 3
+gridInts5 = gridIntsSquare 5
--- day04/forklifts_bad.hs
+++ day04/forklifts_bad.hs
@@ -1,6 +1,6 @@
 import qualified Grid as G
 
-data Cell = Box | Empty
+data Cell = Empty | Box deriving(Eq, Ord)
 
 cellToChar :: Cell -> Char
 cellToChar Box = '@'
@@ -20,7 +20,9 @@ instance Read Cell where
 main' file = do
     contents <- readFile file
     let parsed = parse contents
-    print parsed
+        solved = solve parsed
+    print solved
+
 
 parse :: String -> G.Grid Cell
 parse str = let rows = lines str
@@ -28,3 +30,22 @@ parse str = let rows = lines str
                 h = length rows
                 l = map charToCell $ filter (/='\n') str
              in G.fromList w h l
+
+solve :: G.Grid Cell -> Int
+solve = sum . G.toList . fmap isAccessible . countNeighbours
+
+countNeighbours :: G.Grid Cell ->  G.Grid (Maybe Int)
+countNeighbours g@(G.Grid w h _) = fmap (aux) $
+                                   G.fromList w h $
+                                   G.gridWindowsFill3x3 Empty g
+                                   where aux :: G.Grid Cell -> Maybe Int
+                                         aux g
+                                            | G.gridAt 1 1 g == Just Box =
+                                                Just (sum $ fmap (\(c) -> if c == Box then 1 else 0) g)
+                                            | otherwise = Nothing
+
+isAccessible :: Maybe Int -> Int
+isAccessible c
+    | Nothing <- c = 0
+    | Just n <- c, n <=4 = 1
+    | otherwise = 0
