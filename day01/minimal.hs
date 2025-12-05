245df93b1601eb1d61253178882103da4c482e1c
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-05 12:50:19 +0100

    Day 1: added function type signatures to minimal.hs

--- day01/minimal.hs
+++ day01/minimal.hs
@@ -1,9 +1,13 @@
 import System.Environment
 
+main :: IO ()
 main = getArgs >>= main' . head
 
+main' :: String -> IO ()
 main' file = show . solve1 . parse <$> readFile file >>= putStrLn
 
+parse :: String -> [Int]
 parse = map (read . \(x:xs) -> (if x == 'L' then '-' else ' '):xs) . lines
 
+solve1 :: [Int] -> Int
 solve1 = length . filter (==0) . scanl (\acc v -> (acc+v) `mod` 100) 50
