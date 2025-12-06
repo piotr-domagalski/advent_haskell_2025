8e3cc42eefe3e0ae9a5b4c2d8c282bd7833ee00c
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-06 14:19:38 +0100

    Day 6 task 2

--- day06/math_basic.hs
+++ day06/math_basic.hs
@@ -6,8 +6,11 @@ main' file = do
     let questions = parse contents
         answers = map solveQuestion questions
         total = sum answers
-    print $ answers
-    print $ total
+        questions2 = parse2 contents
+        answers2 = map solveQuestion questions2
+        total2 = sum answers2
+    putStrLn $ "Task 1: " ++ (show total)
+    putStrLn $ "Task 2: " ++ (show total2)
 
 type Operator = Int -> Int -> Int
 type Question = ([Int], Operator)
@@ -16,10 +19,28 @@ parse :: String -> [Question]
 parse string = let columns = L.transpose $ map words $ lines string
                    questions = map (\col -> (map read $ init col, parseOp $ last col)) columns
                 in questions
+
+parse2 :: String -> [Question]
+parse2 string = let columns = spans (not . all isSpace) $ L.transpose $ lines string
+                    questions = map parseCol $ columns
+                 in questions
+
+parseCol :: [String] -> Question
+parseCol col = let nums = map (read . init) col
+                   op   = parseOp $ [last $ head col]
+                in (nums, op)
+
 parseOp :: String -> Operator
 parseOp "*" = (*)
 parseOp "+" = (+)
 parseOp other = error $ "invalid operator: " ++ other
 
+
 solveQuestion :: Question -> Int
 solveQuestion (nums, op) = foldl1 op nums
+
+spans :: (a -> Bool) -> [a] -> [[a]]
+spans _ [] = []
+spans pred list = case span pred list of
+                       (first, [])     -> [first]
+                       (first, _:tail) -> first:spans pred tail
