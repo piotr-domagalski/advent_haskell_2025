65ab270f861621da4cb6dd636575a1507bc0f2d1
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-10 11:46:33 +0100

    day 9 task 2: optimisations (34s -> 4s); still just bruteforce tho 1. sort all rects by area, return first that doesn't overlap the    boundary (34s -> 7s) 2. merge overlapping boundary lines (7s -> 4s)

--- day09/tiles_basic2.hs
+++ day09/tiles_basic2.hs
@@ -1,3 +1,4 @@
+import System.Environment
 import qualified Data.List as L
 
 type Position = (Int, Int)
@@ -12,6 +13,21 @@ newLine :: Position -> Position -> Line
 newLine (fx,fy) (tx,ty)
     | fx == tx = Vert fx (min fy ty) (max fy ty)
     | fy == ty = Hori fy (min fx tx) (max fx tx)
+
+sumLines :: Line -> Line -> Maybe Line
+sumLines (Hori y1 fx1 tx1) (Hori y2 fx2 tx2)
+    | y1 == y2 &&
+     (inRangeIncl (fx1, tx1) fx2 || inRangeIncl (fx1, tx1) tx2 ||
+      inRangeIncl (fx2, tx2) fx1 || inRangeIncl (fx2, tx2) tx1)
+      = Just (Hori y1 (min fx1 fx2) (max tx1 tx2))
+    | otherwise = Nothing
+sumLines (Vert x1 fy1 ty1) (Vert x2 fy2 ty2)
+    | x1 == x2 &&
+     (inRangeIncl (fy1, ty1) fy2 || inRangeIncl (fy1, ty1) ty2 ||
+      inRangeIncl (fy2, ty2) fy1 || inRangeIncl (fy2, ty2) ty1)
+      = Just (Vert x1 (min fy1 fy2) (max ty1 ty2))
+    | otherwise = Nothing
+sumLines _ _ = Nothing
 
 newRect :: Position -> Position -> Rect
 newRect p1@(x1,y1) p2@(x2,y2)
@@ -70,12 +86,16 @@ rotToPlusX a b c = let n = negate $ rotation a b
                     in (negate n, rotate n a, rotate n b, rotate n c)
 
 calcBoundary :: [Position] -> [Line]
-calcBoundary positions@(a:_:_:_:_) = concat $ map (\(a,b) -> [a,b]) $ aux a (drop 1 $ cycle positions)
-    where aux :: Position -> [Position] -> [(Line, Line)]
+calcBoundary positions@(a:_:_:_:_) = aux a (drop 1 $ cycle positions)
+    where aux :: Position -> [Position] -> [Line]
           aux term (a:b:c:d:rest)
             | collinear a b c = aux (if (b == term) then a else term) (a:c:d:rest)
-            | term == a = [aux' a b c]
-            | otherwise = (aux' a b c) : aux term (b:c:d:rest)
+            | term == a = (\(a,b) -> [a,b]) $ aux' a b c
+            | otherwise = let next:tail = aux term (b:c:d:rest)
+                              (prev, cur) = aux' a b c
+                           in case (sumLines cur next) of
+                                Just merged -> prev:merged:tail
+                                Nothing -> prev:cur:next:tail
           aux' :: Position -> Position -> Position -> (Line, Line)
           aux' a b c = let (rot, a'@(ax, ay), b'@(bx, by), c'@(cx, cy)) = rotToPlusX a b c
                            [e1, e2, e3] = if cy > ay
@@ -91,8 +111,6 @@ collinear (x1, y1) (x2, y2) (x3, y3)
     | y1 == y2 && y2 == y3 = True
     | otherwise = False
 
-
-
 solve :: [Position] -> [(Int, Rect)]
 solve positions = let edges = calcBoundary positions
                    in scanl (\curmax x -> max curmax
@@ -102,6 +120,15 @@ solve positions = let edges = calcBoundary positions
                                                             else max (area rect, rect) candidate
                                        ) zeroCandidate positions)
                             ) zeroCandidate positions
+
+solve' positions@(x:tail) = let edges = calcBoundary positions
+                                candidateRects = L.unfoldr allpairs (x, tail)
+                                candidateRects' = L.sortOn (negate . area) $ concat $ candidateRects
+                                maxRect = head $ L.dropWhile (\rect -> any (rect `overlaps`) edges)
+                                                               candidateRects'
+                             in (area maxRect, maxRect)
+    where allpairs (_, []) = Nothing
+          allpairs (x, rest@(y:tail)) = Just ((map (newRect x) rest), (y, tail))
 
 parse :: String -> [Position]
 parse = map (\p -> read $ '(':p++")"). lines
@@ -114,9 +141,14 @@ bruteforce_max (x:rest) = max (bruteforce_max rest)
                                      ) zeroCandidate rest
                               )
 
+main = do
+    (file:_) <- getArgs
+    main' file
 main' file = do
     contents <- readFile file
     let parsed = parse contents
         (ans1, rect1) = bruteforce_max parsed
+        ans2 = solve' parsed
     putStrLn $ "Task 1: " ++ (show ans1)
+    putStrLn $ "Task 2: " ++ (show ans2)
 
