64b68a9a859594d55eeca23d941f8a3f49a10fd3
Piotr Domagalski <pdomagalski02@gmail.com>
2025-12-12 18:56:54 +0100

    day 10 task 2 - failed attempt (reusing the bruteforce code)

--- day10/buttons_basic.hs
+++ day10/buttons_basic.hs
@@ -14,10 +14,11 @@ instance Show State where
               d2c 1 = '#'
               d2c 0 = '.'
               d2c d = error $ "invalid binary digit" ++ (show d)
-              digits :: Integral a => a -> a -> [a]
-              digits _ 0 = []
-              digits b n = let (n', d) = divMod n b
-                            in d:digits b n'
+
+digits :: Integral a => a -> a -> [a]
+digits _ 0 = []
+digits b n = let (n', d) = divMod n b
+              in d:digits b n'
 
 instance Read State where
     readsPrec _ ('[':input) =
@@ -51,24 +52,50 @@ instance Read Button where
                 [] -> []
                 [(list, "")] -> [(Button {getChange = sum $ map (2^) list}, rest)]
                 _ -> []
+    readsPrec _ _ = []
 
+newtype Joltages = Joltages {getJoltages :: [Int]} deriving (Eq, Ord)
+initJoltages :: Int -> Joltages
+initJoltages n = Joltages {getJoltages = replicate n 0}
 
-parse :: String -> [(State, [Button])]
+instance Show Joltages where
+    show joltages = '{':(init $ tail $ show $ getJoltages joltages) ++ "}"
+instance Read Joltages where
+    readsPrec prec ('{':input) = 
+        case span (/= '}') input of
+            (nums, '}':rest) ->
+                case readsPrec prec ('[':nums++"]") of
+                    [(list :: [Int], "")] -> [(Joltages { getJoltages = list }, rest)]
+                    _ -> []
+            _ -> []
+    readsPrec _ _ = []
+
+type Machine = (State, [Button], Joltages)
+parse :: String -> [Machine]
 parse = map parseMachine . lines
 
-parseMachine :: String -> (State, [Button])
+parseMachine :: String -> Machine
 parseMachine str = let (goalStr:rest) = words str
                        goal = read goalStr
                        buttons :: [Button] = map read $ init rest
-                    in (goal, buttons)
+                       joltages :: Joltages = read $ last rest
+                    in (goal, buttons, joltages)
 
-press :: Button -> State -> State
-press b s = State {getState = getState s `xor` getChange b}
+pressLights :: Button -> State -> State
+pressLights b s = State {getState = getState s `xor` getChange b}
 
-solve :: [(State, [Button])] -> [[(Int, [State])]]
-solve machines = map (\(goal, bs) -> solve' 0 goal (map press bs) [initState] S.empty) machines
+pressJoltages :: Button -> Joltages -> Joltages
+pressJoltages b j = Joltages {getJoltages = succ_mask (getChange b) (getJoltages j)}
+    where succ_mask num rest = zipWith (+) (digits 2 num ++ repeat 0) rest
 
-solve' :: Int -> State -> [State -> State] -> [State] -> S.Set State -> [(Int, [State])]
+solve1 :: Machine -> [(Int, [State])]
+solve1 (goal, buttons, _) = solve' 0 goal (map pressLights buttons) [initState] S.empty
+
+solve2 :: Machine -> [(Int, [Joltages])]
+solve2 (_, buttons, goal) = solve' 0 goal (map pressJoltages buttons) [initJolts] S.empty
+    where initJolts = initJoltages $ length $ getJoltages goal
+
+solve' :: (Eq a, Ord a) => Int -> a -> [a -> a] -> [a] -> S.Set a -> [(Int, [a])]
 solve' i goal presses states already_reached =
     let states' = presses <*> states
         already_reached' = already_reached `S.union` (S.fromList states')
@@ -86,8 +113,14 @@ main' :: String -> IO ()
 main' file = do
     contents <- readFile file
     let parsed = parse contents
-        solved1 = solve parsed
+        solved1 = map solve1 parsed
         counts1 = map (fst . last) solved1
-    putStrLn $ (concat $ L.intersperse "+" $ map show counts1)
+        solved2 = map solve2 parsed
+        counts2 = map (fst . last) solved2
+    --putStrLn $ "Task 2 counts: " ++ (unlines $ map (unlines . map show) solved2)
+    --putStrLn $ "Task 1 counts: " ++ (concat $ L.intersperse "+" $ map show counts1)
+    --putStrLn $ "Task 2 counts: " ++ (concat $ L.intersperse "+" $ map show counts2)
     putStrLn $ "Task 1: " ++
                (show $ sum $ counts1)
+    --putStrLn $ "Task 2: " ++
+    --         (show $ sum $ counts2)
